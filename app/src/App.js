9369df1a048009a8ffd14a12fd6f87069a240af0
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-08-24 20:10:45 -0400

    created title and button

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import { Paper, Card, Button, Grid } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    width: '90%',
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  paper: {
+    height: '80vh',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container className={classes.container}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={4} justifyContent='center'>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              <h1>Generate Your Own Axolotl!</h1>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              <Button variant='contained' color='secondary'>
+                {'==> Click Me <=='}
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={12} lg={12} xl={12}>
+              Testing
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
     </div>
   );
 }
